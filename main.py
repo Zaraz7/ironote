ba8a089a99145df448c25339c79d16b9b3700fd0
Nikita Babenko <65152247+Norv139@users.noreply.github.com>
2022-01-23 18:32:34 +0300

    add get request and js render response

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from http.client import responses
+from urllib import response
+from flask import Flask, render_template, request
+from flask_restful import Resource, Api
+
 
 app = Flask(__name__)
+api = Api(app)
+
+class StatusApi(Resource):
+    def get(self):
+        return {"status" : "OK"}
+api.add_resource(StatusApi, '/StatusApi')
+
+class GetHelp(Resource):
+    def get(self):
+        return {"status" : "OK"}
+    def post(self):
+        return request.json
+        
+api.add_resource(GetHelp, '/help')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
