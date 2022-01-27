b3cbe6e86ba59e5bf959c7f9564e547442c543e5
Nikita Babenko <65152247+Norv139@users.noreply.github.com>
2022-01-27 15:04:16 +0300

    Add interface

--- main.py
+++ main.py
@@ -9,20 +9,23 @@ api = Api(app)
 
 class StatusApi(Resource):
     def get(self):
-        return {"status" : "OK"}
+        return {"status" : "Алекс лох"}
 api.add_resource(StatusApi, '/StatusApi')
 
 class GetHelp(Resource):
     def get(self):
         return {"status" : "OK"}
+
     def post(self):
+        print(request.json)
         return request.json
-        
+
 api.add_resource(GetHelp, '/help')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0',port=5005)
